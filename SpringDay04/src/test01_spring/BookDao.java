78a5f8c14b1af67caff4496c28b267ae187aa23b
irrationnelle <drakkarverenis@gmail.com>
2016-12-08 10:21:43 +0900

    commit

---
+++ SpringDay04/src/test01_spring/BookDao.java
@@ -0,0 +1,13 @@
+package test01_spring;
+
+import org.springframework.jdbc.core.JdbcTemplate;
+
+public class BookDao {
+	private JdbcTemplate jdbcTemplate;
+	
+	// 외부에서 jdbcTemplate를 주입받기 위해 생성자나 setters를 만들어줘야 함.
+	
+	public BookDao(JdbcTemplate jdbcTemplate) {
+		this.jdbcTemplate = jdbcTemplate;
+	}
+}
