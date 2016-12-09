fa4e6f95578090fb0eaf499984009b7aee38bf18
irrationnelle <drakkarverenis@gmail.com>
2016-12-09 09:51:28 +0900

    declare and define mapRow method

--- SpringDay04DB/src/test01_spring/BookDao.java
+++ SpringDay04DB/src/test01_spring/BookDao.java
@@ -1,7 +1,11 @@
 package test01_spring;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Component;
 
 import test01_spring.vo.BookVO;
@@ -29,5 +33,28 @@ public class BookDao {
 		
 		// String인지 int인지 자료형을 맞춰줄 필요도 없고 단순히 ? 순서에 맞게 인자값들을 넣어주는 것만 지켜주면 된다
 		return jdbcTemplate.update(sql, book.getTitle(), book.getWriter(), book.getPublisher(), book.getPrice());
+	}
+	
+	
+	class BookMapper implements RowMapper<BookVO> {
+		
+		@Override
+		public BookVO mapRow(ResultSet rs, int arg1) throws SQLException {
+			BookVO book = new BookVO();
+//			book.setBookNum(rs.getInt(1));
+//			book.setTitle(rs.getString(2));
+//			book.setWriter(rs.getString(3));
+//			book.setPrice(rs.getInt(4));
+//			book.setPublisher(rs.getString(5));
+			
+			// 위처럼 안 하고 DBMS의 컬럼명으로 인자를 전달해주는 것 또한 가능
+			book.setBookNum(rs.getInt("book_num"));
+			book.setPrice(rs.getInt("price"));
+			book.setPublisher(rs.getString("publisher"));
+			book.setTitle(rs.getString("title"));
+			book.setWriter(rs.getString("writer"));
+			return null;
+		}
+		
 	}
 }
